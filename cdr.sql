47dbeeef64f08bc455e69074442c6c856e6af9d4
Gu3s5 <42850313+Gu3s5@users.noreply.github.com>
2018-08-31 10:13:37 -0300

    Create cdr.sql

---
+++ cdr.sql
@@ -0,0 +1,54 @@
+create database asteriskcdrdb;
+
+/* Define a senha do usuário como 123456 */
+CREATE USER 'asteriskcdr'@'localhost' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON asteriskcdrdb.* TO 'asteriskcdr'@'%';
+ 
+use asteriskcdrdb;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ 
+--
+ 
+-- Table structure for table `cdr`
+ 
+--
+ 
+DROP TABLE IF EXISTS `cdr`;
+CREATE TABLE `cdr` (
+`calldatestart` datetime NOT NULL default '0000-00-00 00:00:00',
+`calldateend` datetime NOT NULL default '0000-00-00 00:00:00',
+`calldateanswer` datetime NOT NULL default '0000-00-00 00:00:00',
+`clid` varchar(80) NOT NULL default '',
+`src` varchar(80) NOT NULL default '',
+`dst` varchar(80) NOT NULL default '',
+`dcontext` varchar(80) NOT NULL default '',
+`channel` varchar(80) NOT NULL default '',
+`dstchannel` varchar(80) NOT NULL default '',
+`lastapp` varchar(80) NOT NULL default '',
+`lastdata` varchar(80) NOT NULL default '',
+`duration` int(11) NOT NULL default '0',
+`billsec` int(11) NOT NULL default '0',
+`disposition` varchar(45) NOT NULL default '',
+`amaflags` int(11) NOT NULL default '0',
+`accountcode` varchar(20) NOT NULL default '',
+`uniqueid` varchar(32) NOT NULL default '',
+`userfield` varchar(255) NOT NULL default '',
+KEY `calldate` (`calldatestart`),
+KEY `dst` (`dst`),
+KEY `accountcode` (`accountcode`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+ 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
